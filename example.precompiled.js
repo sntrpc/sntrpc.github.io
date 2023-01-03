a1c5b8aaf8e26d61eb59c478a60a507341fc211f
Son <sonvoe@me.com>
2023-01-03 02:56:55 -0800

    commit 1.3

---
+++ example.precompiled.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['example'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "ls\n\n\n\nexit\n:!\n\nexit\n";
+},"useData":true});
+})();
